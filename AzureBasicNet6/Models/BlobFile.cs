b7818f9195cbb418ab97c37f47f7c8afd875d497
JBeni <JBeni@users.noreply.github.com>
2021-12-23 12:34:41 +0200

    create blob file model

---
+++ AzureBasicNet6/Models/BlobFile.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AzureBasicNet6.Models
+{
+    public class BlobFile
+    {
+        public BlobFile()
+        {
+            CreatedDate = DateTime.Now;
+        }
+
+        public string? Name { get; set; }
+        public string? FileUrl { get; set; }
+
+        [NotMapped]
+        [Required(ErrorMessage = "Please Enter Description")]
+        public IFormFile File { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
