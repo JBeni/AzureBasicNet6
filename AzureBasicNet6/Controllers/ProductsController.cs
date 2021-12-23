787854c582cded4d8cb40557639f14465d89e27e
JBeni <JBeni@users.noreply.github.com>
2021-12-23 12:34:31 +0200

    update products controller

--- AzureBasicNet6/Controllers/ProductsController.cs
+++ AzureBasicNet6/Controllers/ProductsController.cs
@@ -1,6 +1,5 @@
 ﻿using AzureBasicNet6.Database;
 using AzureBasicNet6.Models;
-using AzureBasicNet6.Service;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -22,13 +21,11 @@ namespace AzureBasicNet6.Controllers
             _env = env;
         }
 
-        // GET: Products
         public IActionResult Index()
         {
             return View(_context.ProductRepo.GetAll());
         }
 
-        // GET: Products/View/5
         public IActionResult View(int? id)
         {
             if (id == null)
@@ -45,7 +42,6 @@ namespace AzureBasicNet6.Controllers
             return View(product);
         }
 
-        // GET: Products/Create
         public IActionResult Create()
         {
             return View();
@@ -57,32 +53,6 @@ namespace AzureBasicNet6.Controllers
         {
             if (ModelState.IsValid)
             {
-
-                #region Read File Content
-
-                var uploads = Path.Combine(_env.WebRootPath, "uploads");
-                bool exists = Directory.Exists(uploads);
-                if (!exists)
-                    Directory.CreateDirectory(uploads);
-
-                //string fileName = Path.GetFileName(product.File.FileName);
-                //byte[] fileData;
-                //using (var target = new MemoryStream())
-                //{
-                //    product.File.CopyTo(target);
-                //    fileData = target.ToArray();
-                //}
-
-
-                //var fileStream = new FileStream(Path.Combine(uploads, product.File.FileName), FileMode.Create);
-                //string mimeType = product.File.ContentType;
-                //= new byte[product.File.Length];
-
-                BlobStorageService objBlobService = new BlobStorageService(_configuration);
-
-                //product.ImagePath = objBlobService.UploadFileToBlob(product.File.FileName, fileData, mimeType);
-                #endregion
-
                 _context.ProductRepo.Add(product);
                 await _context.SaveChanges();
                 return RedirectToAction(nameof(Index));
@@ -90,7 +60,6 @@ namespace AzureBasicNet6.Controllers
             return View(product);
         }
 
-        // GET: Products/Update/5
         public IActionResult Update(int? id)
         {
             if (id == null)
@@ -106,9 +75,6 @@ namespace AzureBasicNet6.Controllers
             return View(product);
         }
 
-        // POST: Products/Update/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Update(int id, [Bind("ProductId,Name,UnitPrice,Description,ImageName,ImagePath,CreatedDate,UpdatedDate")] Product product)
@@ -141,7 +107,6 @@ namespace AzureBasicNet6.Controllers
             return View(product);
         }
 
-        // GET: Products/Delete/5
         public IActionResult Delete(int? id)
         {
             if (id == null)
@@ -158,14 +123,11 @@ namespace AzureBasicNet6.Controllers
             return View(product);
         }
 
-        // POST: Products/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
             var product = _context.ProductRepo.GetById(id);
-            BlobStorageService objBlob = new BlobStorageService(_configuration);
-            //objBlob.DeleteBlobData(product.ImagePath);
             _context.ProductRepo.Delete(product);
             await _context.SaveChanges();
             return RedirectToAction(nameof(Index));
